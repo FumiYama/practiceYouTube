6cacfc931785ef73182a8f46b3339d45d43b2b70
FumiYama <f.yamanaka1127@gmail.com>
2016-01-06 21:08:36 +0900

    Initial Commit

---
+++ practiceYouTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practiceYouTube
+//
+//  Created by Fumiya Yamanaka on 2016/01/06.
+//  Copyright © 2016年 Fumiya Yamanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
