f7120b6ed7b0a39662abf74bf42c1eb51975ca67
FumiYama <f.yamanaka1127@gmail.com>
2016-01-06 21:28:52 +0900

    first commit

--- practiceYouTube/ViewController.swift
+++ practiceYouTube/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var wv_webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        wv_webView = UIWebView()
+        wv_webView.frame = self.view.bounds
+        
+        self.view.addSubview(wv_webView)
+        
+        let path: NSString = NSBundle.mainBundle().pathForResource("player", ofType: "html")! //ファイルパス生成
+        
+        let request: NSURLRequest = NSURLRequest(URL: NSURL.fileURLWithPath(path as String)) // request生成
+        
+        wv_webView.allowsInlineMediaPlayback = true // fullscreen表示ではなく,inline表示に
+        
+        wv_webView.mediaPlaybackRequiresUserAction = false // 自動的に再生開始
+        
+        wv_webView.loadRequest(request) // load開始
+        
     }
 
     override func didReceiveMemoryWarning() {
